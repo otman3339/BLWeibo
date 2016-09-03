ca051b367cfafb76922600abaf00e9b1959ba063
otman3339 <otman3339@gmail.com>
2016-09-03 14:30:31 +0800

    first commit

---
+++ app/src/main/java/com/bbblue/blweibo/views/NoScrollView.java
@@ -0,0 +1,29 @@
+package com.bbblue.blweibo.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by zeminwang on 16/9/2.
+ */
+public class NoScrollView extends ScrollView {
+
+    public NoScrollView(Context context) {
+        super(context);
+    }
+
+    public NoScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
